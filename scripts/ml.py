b2fdc36cb91d421debfa4d5d00990bbf03cfc766
Tomas Sladecek <tomas.sladecek@geneton.sk>
2021-06-17 11:31:16 +0200

    new summary file

--- scripts/ml.py
+++ scripts/ml.py
@@ -9,9 +9,10 @@ import matplotlib.pyplot as plt
 import seaborn as sns
 
 
-from sklearn.linear_model import LogisticRegression
+from sklearn.linear_model import LogisticRegression, LinearRegression, Ridge
 from sklearn.discriminant_analysis import LinearDiscriminantAnalysis
 from sklearn.svm import SVC
+from sklearn.preprocessing import PolynomialFeatures
 
 from umap import UMAP
 
@@ -59,13 +60,26 @@ sns.scatterplot(x=train_umap[:, 0], y=train_umap[:, 1], hue=train_y)
 # %%
 sns.scatterplot(x="Gestational age", y="first_ff", data=train_X)
 
-# %% MODELS
-results = []
+# %% Regression
+poly = PolynomialFeatures(degree=2)
+train_poly = poly.fit_transform(train_X)
 
-for model in ["logisticregression", "lda", "qda", "svc", "randomforest", "xgboost", "knn"]:
+poly = PolynomialFeatures(degree=2)
+val_poly = poly.fit_transform(val_X)
 
-    res, model = gridsearch(train_X, train_y, val_X, val_y, model,
-                            model_search_space[model])
-    
-    results.append([model] + res.iloc[0, 1:].values.tolist())
+train_X_poly = ss.fit_transform(train_poly)
+val_X_poly = ss.transform(val_poly)
 
+# %%
+reducer = UMAP(n_neighbors=5, min_dist=0.01, random_state=1618)
+
+reducer.fit(train_X_poly)
+
+train_umap = reducer.transform(train_X_poly)
+val_umap = reducer.transform(val_X_poly)
+
+
+sns.scatterplot(x=val_umap[:, 0], y=val_umap[:, 1], hue=val_y)
+sns.scatterplot(x=train_umap[:, 0], y=train_umap[:, 1], hue=train_y)
+
+# %%
